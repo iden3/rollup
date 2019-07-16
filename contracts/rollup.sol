5705ebbd6bfd6d9108bd12ad252fcd8ac6120863
krlosMata <krlosmata@gmail.com>
2019-07-16 11:34:44 +0200

    wip sm

--- contracts/rollup.sol
+++ contracts/rollup.sol
@@ -1,110 +1,215 @@
 
-pragma solidity ^0.5;
+pragma solidity ^0.5.0;
+import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol"; 
+
+/**
+ * @dev Define interface Verifier contract
+ */
+contract Verifier {
+  function verifyProof( uint[2] memory a, uint[2][2] memory b,
+    uint[2] memory c, uint[10] memory input) view public returns (bool r);
+}
+
+contract Rollup is Ownable {
+
+  // External contracts used
+  Verifier verifier;
+
+  address owner;
+
+  // Minim collateral required to commit for a block
+  uint constant MIN_COMMIT_COLLATERAL = 1 ether;
+
+  // Blocks that the operator can forge
+  uint constant MAX_COMMIT_BLOCKS = 150; 
+
+  /**
+   * @dev global variales
+   * Function: Commit a block
+   */
+  // Operator that is commited to forge the block
+  address commitedOperator;     
+  // Balance staked for operators at the time to commit a block
+  uint balanceCommited;
+  // Block where the commit is not valid any more
+  uint commitBlockExpires;
+  // Root commited
+  bytes32 rootCommited;
 
 
-contract Rollup is Owned {
+  bytes32[] stateRoots;
+  bytes32[] exitRoots;
 
-    uint constant MIN_COMMIT_COLLATERAL = 1 ether; // Minim collateral required to commit for a block
-    uint constant MAX_COMMIT_BLOCKS = 150; // Blocks that the operator can forge
+  // List of ERC20 tokens
+  address[] tokens;
 
-    byte32[] stateRoots;
-    byte32[] exitRoots;
+  // Last account id to add into the side chain
+  uint lastAssignedIdx;
 
-    address[] tokens;               // List of ERC20 tokens
+  // Hash of the OnChain TXs list that will be mined in the next block
+  bytes32 miningOnChainTxsHash;   
 
-    address commitedOperator;       // Operator that is commited to forge the block
-    uint commitBlockExpires;        // Block where the commit is not valid any more
+  // Hash of the OnChain TXs list that will be mined in two blocks
+  // New onChain TX goes to this list, either deposit or forceWithdraw
+  bytes32 fillingOnChainTxsHash;
 
-    uint lastAssignedIdx;
+  /**
+   * @dev constructor
+   */
+  constructor(address _verifier) public {
+    verifier = Verifier(_verifier);
+    owner = msg.sender;
+  }
 
-    byte32 miningOnChainTxsHash;    // Hash of the OnChain TXs list that will be mined in the next block
-    byte32 fillingOnChainTxsHash;   // Hash of the OnChain TXs list that will be mined in two blocks
-                                    // New onChain TX goes to this list
-
-
-    function commitToBlock(
-        bytes32 newRoot
-    ) public payable {
-    }
-
-    function forgeBlock(
-        bytes32 newStateRoot,
-        bytes32 exitRoot,
-        uint32[2] memory feePlan,
-        uint32 nTxPerCoin,
-        uint[2] memory proofA,
-        uint[2][2] memory proofB,
-        uint[2] memory proofC,
-        bytes memory compressedTxs,
-        address beneficiary
-    ) public {
-
-        // Public Parameters of the curcuit
-        // newStateRoot,
-        // ExitRoot
-        // feePlan[2]
-        // nTxPerCoin
-        // Hash(compressedTxs)
-        // miningOnChainTxsHash
-        // beneficiary
+  /**
+   * @dev check if the last root commited have not been forged
+   * by checking the blocks that the operator is able to forge
+   * the block
+   */
+  function resetCommitState() public {
+    require( block.number > commitBlockExpires );
+    require( commitedOperator != address(0) );
+    // burn balance staked by the operator
+    address(0).transfer(balanceCommited);
+    // reset commit variables
+    balanceCommited = 0;
+    commitedOperator = address(0);
+  }
 
 
-        miningOnChainTxsHash = fillingOnChainTxsHash;
-        fillingOnChainTxsHash = 0;
-    }
+  /**
+   * @dev operator commits the new block along with a deposit
+   * operator has a certain amount of blocks to validate the bock commited
+   * fees and deposit are paied to operator if block is finally added succesfully
+   * otherwise, deposit is burned and new block commit would be available again 
+   * @param newRoot new sidechain root commited
+   */
+  function commitToBlock( bytes32 newRoot ) public payable {
+    // Ensure there is no current block commited
+    require( commitedOperator == address(0) );
+    require( block.number > commitBlockExpires );
+    // Ensure msg.sender has enough balance to commit a new root
+    require( msg.value >= MIN_COMMIT_COLLATERAL );
+    // Stake balance
+    balanceCommited = msg.value; 
+    // Set last block to forge the root
+    commitBlockExpires = block.number + MAX_COMMIT_BLOCKS;
+    // Save operator that commited the root
+    commitedOperator = msg.sender;
+    // Save the root
+    rootCommited = newRoot;
+  }
 
-    // TODO: Deposit fees?
-    function deposit(
-        uint depositAmount,
-        uint token,
-        uint[2] babyPubKey,
-        uint to,                // In the TX deposit, it allows to do a send during deposit
-        uint sendAmount
-    ) payable public {
+  /**
+   * @dev Checks proof given by the operator
+   * forge the block if succesfull, burn operator stake otherwise
+   * @param proofA zero knowledge input
+   * @param proofB zero knowledge input
+   * @param proofC zero knowledge input
+   * @param newStateRoot new root to add in rootStates
+   * @param exitRoot root of all exit transaction
+   * @param feePlan fee operator plan
+   * @param nTxPerCoin number of transmission per coin in order to calculate total fees
+   * @param compressedTxs data availability to maintain sidecain
+   * @param beneficiary address destination to receive fee transactions 
+   */
+  function forgeBlock( uint[2] memory proofA, uint[2][2] memory proofB, uint[2] memory proofC,
+    bytes32 newStateRoot, bytes32 exitRoot, uint32[2] memory feePlan, uint32 nTxPerCoin,
+    bytes memory compressedTxs, address beneficiary) public {
+    // Public Parameters of the circuit
+      // [] newStateRoot,
+      // [] exitRoot
+      // [] feePlan[2]
+      // [] nTxPerCoin
+      // [] Hash(compressedTxs)
+      // [] miningOnChainTxsHash
+      // [] beneficiary
+    
+      uint[] memory inputs;
 
-        lastAssignedIdx++;
-        fillingOnChainTxsHash = hash(fillingOnChainTxsHash, thisTx);
-    }
-
-    // TODO: Withdraw fee?
-    function withdraw(
-        uint exitBlock,
-        uint amount,
-        uint coin,
-        uint[2] babyPubKey,
-        bytes merkleProof,
-        bytes babySignature,
-        address dest
-    ) {
-
-    }
-
-    // TODO: Withdraw fee?
-    function forceWithdraw(
-        uint idx,
-        uint amount,
-        bytes babySignature,
-    ) {
-
-        fillingOnChainTxsHash = hash(fillingOnChainTxsHash, thisTx);
-    }
-
-    function addToken(address newToken) onlyOwner public {
-        assert(tokens.length<0xFFFF);
-        tokens.push(newToken);
-    }
-
-//////////////
-// Viewers
-/////////////
-
-    function getRoot(uint idx) public view returns (bytes32) {
-        return roots[idx];
-    }
+      
 
 
-    /// @return Total number of blocks mined
-    function getDepth() public view returns (uint) {
-        return roots.length;
-    }
+    // Verify circuit
+      // code public inputs to have uint[x]
+        // hash of compressed tx
+        // get miningOnChainTxsHash
+    
+    // Calculate fees
+      // feePlan & nTxPerCoin
+      // Send fees to beneficiary
+      
+    // EXpose transacction through events ?
+
+    //
+      
+
+
+      miningOnChainTxsHash = fillingOnChainTxsHash;
+      fillingOnChainTxsHash = 0;
+  }
+
+  // TODO: Deposit fees?
+  function deposit(
+      uint depositAmount,
+      uint token,
+      uint[2] memory babyPubKey,
+      uint to,                // In the TX deposit, it allows to do a send during deposit
+      uint sendAmount
+  ) payable public {
+
+      lastAssignedIdx++;
+      // TODO: pseudo-code
+      // fillingOnChainTxsHash = hash(fillingOnChainTxsHash, thisTx);
+  }
+
+  // TODO: Withdraw fee?
+  function withdraw(
+      uint exitBlock,
+      uint amount,
+      uint coin,
+      uint[2] memory babyPubKey,
+      bytes memory merkleProof,
+      bytes memory babySignature,
+      address dest
+  ) public {
+
+  }
+
+  // TODO: Withdraw fee?
+  function forceWithdraw(
+      uint idx,
+      uint amount,
+      bytes memory babySignature
+  ) public {
+
+    // TODO: pseudo-code
+    // fillingOnChainTxsHash = hash(fillingOnChainTxsHash, thisTx);
+  }
+
+  function addToken(address newToken) public onlyOwner {
+      assert(tokens.length<0xFFFF);
+      tokens.push(newToken);
+  }
+
+  //////////////
+  // Viewers
+  /////////////
+
+  /**
+   * @dev Retrieve root given its block depth
+   * @return root
+   */
+  function getRoot(uint id) public view returns (bytes32) {
+    return stateRoots[id];
+  }
+
+
+  /**
+   * @dev Retrieve total number of blocks mined
+   * @return Total number of blocks mined
+   */
+  function getDepth() public view returns (uint) {
+    return stateRoots.length;
+  }
 }
