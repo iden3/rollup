62ddcf0e8ba88174a1ac43c2ba2eb7af69f237e7
Laia <laisolizq@gmail.com>
2019-07-30 16:40:15 +0200

    minor changes api-client

--- rollup-cli/src/api-client.js
+++ rollup-cli/src/api-client.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express();
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-app.post("/offchain/send", function(req, res) {
-    let send = {
-        from: req.body.from,
-        to: req.body.to,
-        amount: req.body.amount
-    }
-    if(send.from === undefined || send.to === undefined || send.amount === undefined) {
-        res.sendStatus(500);
-    } else {
-        res.sendStatus(200);
-    }
-})
+app.post('/offchain/send', (req, res) => {
+  const send = {
+    from: req.body.from,
+    to: req.body.to,
+    amount: req.body.amount,
+  };
+  if (send.from === undefined || send.to === undefined || send.amount === undefined) {
+    res.sendStatus(500);
+  } else {
+    res.sendStatus(200);
+  }
+});
 
-app.listen(9000, function() {
-    console.log('App listening on port 9000')
-})
+app.listen(9000, () => {
+  console.log('App listening on port 9000');
+});
