a6d5cce499535219303b2d50958886e677cf8ab9
Laia <laisolizq@gmail.com>
2019-07-31 17:18:18 +0200

    comments and add json path

--- rollup-cli/src/wallet.js
+++ rollup-cli/src/wallet.js
@@ -11,15 +11,25 @@ const web3 = new Web3();
 const iden3 = require('../node_modules/iden3/index');
 const eddsa = iden3.crypto.eddsaBabyJub;
 
-async function _writeFile(txs) {
+/**
+ * Function to write a json file
+ * @param {Object} txs - Data for json
+ * @param {string} path - json path 
+ */
+async function _writeFile(txs, path) {
   await new Promise((resolve, reject) => {
-    fs.writeFile('wallet-rollup-cli.json', JSON.stringify(txs), 'utf8', (err) => {
-      if (err) reject(err)
-      else resolve()
-    })
-  })
+    fs.writeFile(path, JSON.stringify(txs), 'utf8', (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
 }
 
+/**
+ * Function to read a json file
+ * @param {Object} path - json path
+ * @returns {Object} - Data from json
+ */
 async function readFile(path) {
   let info;
   await new Promise((resolve) => {
@@ -32,17 +42,29 @@ async function readFile(path) {
   return JSON.parse(info);
 }
 
-async function _writeFileEthWallet(wallet, pass) { 
+/**
+ * Function to write a ethereum wallet into json file
+ * @param {Object} wallet - Ethereum wallet
+ * @param {string} pass - Passphrase
+ * @param {string} path - json path
+ */
+async function _writeFileEthWallet(wallet, pass, path) {
   const pubKey = wallet.signingKey.publicKey;
   const encPrivateKey = web3.eth.accounts.encrypt(wallet.signingKey.privateKey, pass);
   const obj = {};
   obj.pubKey = pubKey;
   obj.encPrivateKey = encPrivateKey;
   obj.mnemonic = wallet.signingKey.mnemonic;
-  await _writeFile(obj);
+  await _writeFile(obj, path);
 }
 
-async function _writeFileBabyJubWallet(mnemonic, pass) { 
+/**
+ * Function to write a babyjub wallet into json file
+ * @param {Object} mnemonic
+ * @param {string} pass - Passphrase
+ * @param {string} path - json path
+ */
+async function _writeFileBabyJubWallet(mnemonic, pass, path) {
   const seed = bip39.mnemonicToSeedSync(mnemonic);
   const root = hdkey.fromMasterSeed(seed);
   const pathBaby = "m/44'/60'/0'/0/0/0";
@@ -54,27 +76,55 @@ async function _writeFileBabyJubWallet(mnemonic, pass) {
   obj.pubKey = '0x' + publicKeyHex;
   obj.encPrivateKey = web3.eth.accounts.encrypt('0x' + priv1, pass);
   obj.mnemonic = mnemonic;
-  await _writeFile(obj)
+  await _writeFile(obj, path);
 }
 
-async function createEth(pass) {
+/**
+ * Function to create ethereum wallet
+ * @param {string} pass - Passphrase
+ * @param {string} path - json path
+ */
+async function createEth(pass, path) {
   const mnemonic = bip39.generateMnemonic();
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-  await _writeFileEthWallet(wallet, pass);
+  await _writeFileEthWallet(wallet, pass, path);
 }
 
-async function importEth(mnemonic, pass) {
+/**
+ * Function to import ethereum wallet
+ * @param {menmonic} mnemonic
+ * @param {string} pass - Passphrase
+ * @param {string} path - json path
+ */
+async function importEth(mnemonic, pass, path) {
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-  await _writeFileEthWallet(wallet, pass);
+  await _writeFileEthWallet(wallet, pass, path);
 }
 
-async function createBabyJub(pass) {
+/**
+ * Function to create babyjub wallet
+ * @param {string} pass - Passphrase
+ * @param {string} path - json path
+ */
+async function createBabyJub(pass, path) {
   const mnemonic = bip39.generateMnemonic();
-  await _writeFileBabyJubWallet(mnemonic, pass);
+  await _writeFileBabyJubWallet(mnemonic, pass, path);
 }
 
-async function importBabyJub(mnemonic, pass) {
-  await _writeFileBabyJubWallet(mnemonic, pass);
-} 
+/**
+ * Function to import babyjub wallet
+ * @param {menmonic} mnemonic
+ * @param {string} pass - Passphrase
+ * @param {string} path - json path
+ */
+async function importBabyJub(mnemonic, pass, path) {
+  await _writeFileBabyJubWallet(mnemonic, pass, path);
+}
 
-module.exports = { createEth, importEth, readFile, createBabyJub, importBabyJub };
+module.exports = {
+  createEth,
+  importEth,
+  readFile,
+  createBabyJub,
+  importBabyJub,
+};
--- rollup-cli/src/wallet.test.js
+++ rollup-cli/src/wallet.test.js
@@ -1,42 +1,42 @@
 
 // import { describe, it } from 'mocha';
 const Web3 = require('web3');
-const web3 = new Web3();
 const ethers = require('ethers');
 const chai = require('chai');
 const { createEth, importEth, readFile, createBabyJub, importBabyJub } = require('./wallet');
 
+const web3 = new Web3();
 const { expect } = chai;
 const pass = 'pass';
 
 describe('wallet Eth', () => {
+  // Import ethereum wallet from mnemonic
   it('import mnemonic', async () => {
-    await importEth('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point', pass);
+    await importEth('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point', pass, 'wallet-rollup-cli.json');
     const data = await readFile('wallet-rollup-cli.json');
     expect(data.mnemonic).to.be.equal('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point');
-    const decAccount = web3.eth.accounts.decrypt(data.encPrivateKey, pass);
-    expect(decAccount.address.toLowerCase()).to.be.equal('0x7defb80823d81b872e37e3285d30aca1ec3a6500');
-    expect(decAccount.privateKey).to.be.equal('0xc7d654d2eef522e3ad0eae464925d88b218cfaa3cd4f2499314047989e88d83d');
+    expect(data.pubKey).to.be.equal('0x0493b9c8c4b837eeac7775913ee40d1ef3c7b90ec3ee74108f62d35ad69a940e1ff2e8ab35899df986c4e468d210fa09da30017309252358c3101160dd2a2bd377');
   });
-
+  // Create ethereum wallet
   it('create', async () => {
-    await createEth(pass);
+    await createEth(pass, 'wallet-rollup-cli.json');
     const data = await readFile('wallet-rollup-cli.json');
     expect(data.mnemonic).to.be.not.equal(undefined);
     expect(data.pubKey).to.be.not.equal(undefined);
     expect(data.encPrivateKey).to.be.not.equal(undefined);
   });
-
+  // Create and Import ethereum wallet
   it('flow', async () => {
-    await createEth(pass);
+    await createEth(pass, 'wallet-rollup-cli.json');
     const createWallet = await readFile('wallet-rollup-cli.json');
-    await importEth(createWallet.mnemonic, pass);
+    await importEth(createWallet.mnemonic, pass, 'wallet-rollup-cli.json');
     const importWallet = await readFile('wallet-rollup-cli.json');
     expect(createWallet.encPrivateKey.address).to.be.equal(importWallet.encPrivateKey.address);
   });
 });
 
 describe('Decrypt', () => {
+  // Decrypt ethereum wallet
   it('decrypt eth account', async () => {
     const data = await readFile('wallet-rollup-cli.json');
     const decAccount = web3.eth.accounts.decrypt(data.encPrivateKey, pass);
@@ -46,25 +46,26 @@ describe('Decrypt', () => {
 });
 
 describe('wallet BabyJub', () => {
+  //Import babyjub wallet from mnemonic
   it('import mnemonic', async () => {
-    await importBabyJub('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point', pass);
+    await importBabyJub('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point', pass, 'wallet-rollup-cli.json');
     const data = await readFile('wallet-rollup-cli.json');
     expect(data.mnemonic).to.be.equal('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point');
-    //const decAccount = web3.eth.accounts.decrypt(data.encPrivateKey, pass);
+    expect(data.pubKey).to.be.equal('0x8d5a21a3b0568c16539b3c8fbe651df4983994aee21756c1eaae89e9a70a5383');
   });
-
+  //Create babyjub wallet
   it('create', async () => {
-    await createBabyJub(pass);
+    await createBabyJub(pass, 'wallet-rollup-cli.json');
     const data = await readFile('wallet-rollup-cli.json');
     expect(data.mnemonic).to.be.not.equal(undefined);
     expect(data.pubKey).to.be.not.equal(undefined);
     expect(data.encPrivateKey).to.be.not.equal(undefined);
   });
-
+  //Create and import babyjub wallet
   it('flow', async () => {
-    await createBabyJub(pass);
+    await createBabyJub(pass, 'wallet-rollup-cli.json');
     const createWallet = await readFile('wallet-rollup-cli.json');
-    await importBabyJub(createWallet.mnemonic, pass);
+    await importBabyJub(createWallet.mnemonic, pass, 'wallet-rollup-cli.json');
     const importWallet = await readFile('wallet-rollup-cli.json');
     expect(createWallet.encPrivateKey.address).to.be.equal(importWallet.encPrivateKey.address);
   });
