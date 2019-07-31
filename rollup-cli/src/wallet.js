6d8411700d09f7b893b3d18c6b48093cc8f775e0
Laia <laisolizq@gmail.com>
2019-07-31 14:45:10 +0200

    add create and import babyjub

--- rollup-cli/src/wallet.js
+++ rollup-cli/src/wallet.js
@@ -3,39 +3,27 @@ const ethers = require('ethers');
 const fs = require('fs');
 const bip39 = require('bip39');
 const Web3 = require('web3');
+const hdkey = require('hdkey');
 
 const web3 = new Web3();
 // const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 // const web3 = new Web3(provider);
-/* const iden3 = require('../node_modules/iden3/index');
-const eddsa = iden3.crypto.eddsaBabyJub; */
+const iden3 = require('../node_modules/iden3/index');
+const eddsa = iden3.crypto.eddsaBabyJub;
 
-function _writeFile(txs) {
-  fs.writeFile('wallet-rollup-cli.json', JSON.stringify(txs), 'utf8', (err) => {
-    if (err) throw err;
-  });
-}
-
-function _writeFileEthWallet(wallet, pass) { 
-  const pubKey = wallet.signingKey.publicKey;
-  const encPrivateKey = web3.eth.accounts.encrypt(wallet.signingKey.privateKey, pass);
-  const obj = {};
-  obj.pubKey = pubKey;
-  obj.encPrivateKey = encPrivateKey;
-  obj.mnemonic = wallet.signingKey.mnemonic;
-  _writeFile(obj);
-}
-/* function readFile() {
-  fs.readFile('wallet-rollup-cli.json', (err, data) => {
-    if (err) throw err;
-    return JSON.parse(data);
+async function _writeFile(txs) {
+  await new Promise((resolve, reject) => {
+    fs.writeFile('wallet-rollup-cli.json', JSON.stringify(txs), 'utf8', (err) => {
+      if (err) reject(err)
+      else resolve()
+    })
   })
-} */
+}
 
-async function readFile() {
+async function readFile(path) {
   let info;
   await new Promise((resolve) => {
-    fs.readFile('wallet-rollup-cli.json', (err, data) => {
+    fs.readFile(path, (err, data) => {
       if (err) throw err;
       info = data;
       resolve(info);
@@ -44,23 +32,49 @@ async function readFile() {
   return JSON.parse(info);
 }
 
-function createEth(pass) {
+async function _writeFileEthWallet(wallet, pass) { 
+  const pubKey = wallet.signingKey.publicKey;
+  const encPrivateKey = web3.eth.accounts.encrypt(wallet.signingKey.privateKey, pass);
+  const obj = {};
+  obj.pubKey = pubKey;
+  obj.encPrivateKey = encPrivateKey;
+  obj.mnemonic = wallet.signingKey.mnemonic;
+  await _writeFile(obj);
+}
+
+async function _writeFileBabyJubWallet(mnemonic, pass) { 
+  const seed = bip39.mnemonicToSeedSync(mnemonic);
+  const root = hdkey.fromMasterSeed(seed);
+  const pathBaby = "m/44'/60'/0'/0/0/0";
+  const addrNode = root.derive(pathBaby);
+  const priv1 = addrNode._privateKey.toString('hex');
+  const privateKey = new eddsa.PrivateKey(addrNode._privateKey);
+  const publicKeyHex = privateKey.public().toString();
+  const obj = {};
+  obj.pubKey = '0x' + publicKeyHex;
+  obj.encPrivateKey = web3.eth.accounts.encrypt('0x' + priv1, pass);
+  obj.mnemonic = mnemonic;
+  await _writeFile(obj)
+}
+
+async function createEth(pass) {
   const mnemonic = bip39.generateMnemonic();
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-  _writeFileEthWallet(wallet, pass);
+  await _writeFileEthWallet(wallet, pass);
 }
 
-function importEth(mnemonic, pass) {
+async function importEth(mnemonic, pass) {
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-  _writeFileEthWallet(wallet, pass);
+  await _writeFileEthWallet(wallet, pass);
 }
 
-module.exports = { createEth, importEth, readFile };
-
-/* function createBabyJub(pass) {
+async function createBabyJub(pass) {
+  const mnemonic = bip39.generateMnemonic();
+  await _writeFileBabyJubWallet(mnemonic, pass);
 }
-function importBabyJub(mnemonic, pass) {
-} */
-// createEth('hola')
-// importEth('alley topic basic obscure hotel trust concert loyal design second this oxygen', 'hola')
-// readFile()
+
+async function importBabyJub(mnemonic, pass) {
+  await _writeFileBabyJubWallet(mnemonic, pass);
+} 
+
+module.exports = { createEth, importEth, readFile, createBabyJub, importBabyJub };
--- rollup-cli/src/wallet.test.js
+++ rollup-cli/src/wallet.test.js
@@ -4,48 +4,68 @@ const Web3 = require('web3');
 const web3 = new Web3();
 const ethers = require('ethers');
 const chai = require('chai');
-const { createEth, importEth, readFile } = require('./wallet');
+const { createEth, importEth, readFile, createBabyJub, importBabyJub } = require('./wallet');
 
 const { expect } = chai;
 const pass = 'pass';
 
 describe('wallet Eth', () => {
   it('import mnemonic', async () => {
-    importEth('alley topic basic obscure hotel trust concert loyal design second this oxygen', pass);
-    const data = await readFile();
-    expect(data.mnemonic).to.be.equal('alley topic basic obscure hotel trust concert loyal design second this oxygen');
+    await importEth('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point', pass);
+    const data = await readFile('wallet-rollup-cli.json');
+    expect(data.mnemonic).to.be.equal('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point');
+    const decAccount = web3.eth.accounts.decrypt(data.encPrivateKey, pass);
+    expect(decAccount.address.toLowerCase()).to.be.equal('0x7defb80823d81b872e37e3285d30aca1ec3a6500');
+    expect(decAccount.privateKey).to.be.equal('0xc7d654d2eef522e3ad0eae464925d88b218cfaa3cd4f2499314047989e88d83d');
   });
 
   it('create', async () => {
-    createEth(pass);
-    const data = await readFile();
+    await createEth(pass);
+    const data = await readFile('wallet-rollup-cli.json');
     expect(data.mnemonic).to.be.not.equal(undefined);
     expect(data.pubKey).to.be.not.equal(undefined);
     expect(data.encPrivateKey).to.be.not.equal(undefined);
   });
 
   it('flow', async () => {
-    createEth(pass);
-    const createWallet = await readFile();
-    importEth(createWallet.mnemonic, pass);
-    const importWallet = await readFile();
+    await createEth(pass);
+    const createWallet = await readFile('wallet-rollup-cli.json');
+    await importEth(createWallet.mnemonic, pass);
+    const importWallet = await readFile('wallet-rollup-cli.json');
     expect(createWallet.encPrivateKey.address).to.be.equal(importWallet.encPrivateKey.address);
   });
 });
 
 describe('Decrypt', () => {
   it('decrypt eth account', async () => {
-    const data = await readFile();
+    const data = await readFile('wallet-rollup-cli.json');
     const decAccount = web3.eth.accounts.decrypt(data.encPrivateKey, pass);
     const wallet = ethers.Wallet.fromMnemonic(data.mnemonic);
     expect(decAccount.address).to.be.equal(wallet.address);
   });
 });
-/* describe('wallet BabyJub', () => {
-  it('import mnemonic', () => {      
+
+describe('wallet BabyJub', () => {
+  it('import mnemonic', async () => {
+    await importBabyJub('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point', pass);
+    const data = await readFile('wallet-rollup-cli.json');
+    expect(data.mnemonic).to.be.equal('pepper jazz awkward like unhappy opera genuine jazz flash benefit country point');
+    //const decAccount = web3.eth.accounts.decrypt(data.encPrivateKey, pass);
   });
-  it('create', () => {
+
+  it('create', async () => {
+    await createBabyJub(pass);
+    const data = await readFile('wallet-rollup-cli.json');
+    expect(data.mnemonic).to.be.not.equal(undefined);
+    expect(data.pubKey).to.be.not.equal(undefined);
+    expect(data.encPrivateKey).to.be.not.equal(undefined);
   });
-  it('flow', () => {
+
+  it('flow', async () => {
+    await createBabyJub(pass);
+    const createWallet = await readFile('wallet-rollup-cli.json');
+    await importBabyJub(createWallet.mnemonic, pass);
+    const importWallet = await readFile('wallet-rollup-cli.json');
+    expect(createWallet.encPrivateKey.address).to.be.equal(importWallet.encPrivateKey.address);
   });
-}); */
+});
