19d9c7447777515118f9a9b2c5008e420d814fa0
Laia <laisolizq@gmail.com>
2019-09-19 15:55:38 +0200

    cli-utils

--- rollup-cli/src/cli-utils.js
+++ rollup-cli/src/cli-utils.js
@@ -1,20 +1,17 @@
 const Db = require('./db');
 const { send, deposit } = require('./actions/cli-actions');
 
-function loadDb(obj) {
-  
+function sendTx(urlOperator, from, to, amount, walletBabyjub, passString) {
+  console.log(urlOperator + "\n" + from + "\n" + to + "\n" + amount + "\n" + walletBabyjub + "\n" + passString);
+  //send(urlOperator, from, to, amount, walletBabyjub, passString)
 }
 
-function sendTx(walletPath, passString, to, amount, operator) {
-  send(walletPath, passString, to, amount, operator)
-}
-
-function depositTx(walletPath, passString) {
-  deposit(walletPath, passString)
+function depositTx(node, address, amount, tokenid, walletEth, passString, walletBabyjub, abi) {
+  console.log(node + "\n" + address + "\n" + amount + "\n" + tokenid + "\n" + walletEth + "\n" + passString + "\n" + walletBabyjub + "\n" + "abi");
+  //deposit(node, address, amount, tokenid, walletEth, passString, walletBabyjub, abi);
 }
 
 module.exports = {
-  loadDb,
   sendTx,
   depositTx
 };
