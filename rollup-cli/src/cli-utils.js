0dc7ca4d98a4e1a729f3d4d915f46baec5e1dfa0
Laia <laisolizq@gmail.com>
2019-09-20 11:12:49 +0200

    delete cli-utils

--- rollup-cli/src/cli-utils.js
+++ rollup-cli/src/cli-utils.js
@@ -2,12 +2,14 @@ const Db = require('./db');
 const { send, deposit } = require('./actions/cli-actions');
 
 function sendTx(urlOperator, from, to, amount, walletBabyjub, passString) {
-  console.log(urlOperator + "\n" + from + "\n" + to + "\n" + amount + "\n" + walletBabyjub + "\n" + passString);
+  //console.log(urlOperator + "\n" + from + "\n" + to + "\n" + amount + "\n" + walletBabyjub + "\n" + passString);
   //send(urlOperator, from, to, amount, walletBabyjub, passString)
+  console.log("send")
 }
 
 function depositTx(node, address, amount, tokenid, walletEth, passString, walletBabyjub, abi) {
-  console.log(node + "\n" + address + "\n" + amount + "\n" + tokenid + "\n" + walletEth + "\n" + passString + "\n" + walletBabyjub + "\n" + "abi");
+  console.log("deposit")
+  //console.log(node + "\n" + address + "\n" + amount + "\n" + tokenid + "\n" + walletEth + "\n" + passString + "\n" + walletBabyjub + "\n" + "abi");
   //deposit(node, address, amount, tokenid, walletEth, passString, walletBabyjub, abi);
 }
 
