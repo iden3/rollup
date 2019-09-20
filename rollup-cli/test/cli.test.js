539f98fc1b5da4e5366bb619e96c83b2e63bf91f
Laia <laisolizq@gmail.com>
2019-09-20 11:21:04 +0200

    cli-laia -> cli

--- rollup-cli/test/cli.test.js
+++ rollup-cli/test/cli.test.js
@@ -8,14 +8,14 @@ const { expect } = chai;
 
 describe("PRINT KEYS", () => {
   it("printkeys command", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js printkeys');
+    const out = process.exec('cd ..; node cli.js printkeys');
     out.stdout.on('data', (data) => {
       expect(data.toString()).to.be.equal('The following keys have been found:\n');
       done();
     });
   });
   it("printkeys command error", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js printkeyss');
+    const out = process.exec('cd ..; node cli.js printkeyss');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
@@ -25,7 +25,7 @@ describe("PRINT KEYS", () => {
 
 describe("CREATE KEYS RANDOM", () => {
   it("createkeys random", (done) => {
-    process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --pass password --path ./src/walletRollup.json', () => {
+    process.exec('cd ..; node cli.js createkeys --keytype rollup --pass password --path ./src/walletRollup.json', () => {
       var readWallet = undefined;
       readWallet = fs.readFileSync('../src/walletRollup.json', "utf8");
       expect(readWallet).to.not.be.equal(undefined);
@@ -34,7 +34,7 @@ describe("CREATE KEYS RANDOM", () => {
     });
   });
   it("createkeys random default path", (done) => {
-    process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --pass password', () => {
+    process.exec('cd ..; node cli.js createkeys --keytype rollup --pass password', () => {
       var readWallet = undefined;
       readWallet = fs.readFileSync(walletPathDefault, "utf8");
       expect(readWallet).to.not.be.equal(undefined);
@@ -43,14 +43,14 @@ describe("CREATE KEYS RANDOM", () => {
     });
   });
   it("createkeys error password", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --path ./src/walletRollup.json');
+    const out = process.exec('cd ..; node cli.js createkeys --keytype rollup --path ./src/walletRollup.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("createkeys error keytype", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --pass password --path ./src/walletRollup.json');
+    const out = process.exec('cd ..; node cli.js createkeys --pass password --path ./src/walletRollup.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
@@ -60,7 +60,7 @@ describe("CREATE KEYS RANDOM", () => {
 
 describe("CREATE KEYS MNEMONIC", () => {
   it("createkeys mnemonic", (done) => {
-    process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --pass password --path ./src/walletRollupMnemonic.json', () => {
+    process.exec('cd ..; node cli.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --pass password --path ./src/walletRollupMnemonic.json', () => {
       var readWalletMnemonic = undefined;
       readWalletMnemonic = fs.readFileSync('../src/walletRollupMnemonic.json', "utf8");
       expect(JSON.parse(readWalletMnemonic).ethWallet.address).to.be.equal("ea7863f14d1a38db7a5e937178fdb7dfa9c96ed7");
@@ -69,7 +69,7 @@ describe("CREATE KEYS MNEMONIC", () => {
     });
   });
   it("createkeys mnemonic default path", (done) => {
-    process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --pass password', () => {
+    process.exec('cd ..; node cli.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --pass password', () => {
       var readWalletMnemonic = undefined;
       readWalletMnemonic = fs.readFileSync(walletPathDefault, "utf8");
       expect(JSON.parse(readWalletMnemonic).ethWallet.address).to.be.equal("ea7863f14d1a38db7a5e937178fdb7dfa9c96ed7");
@@ -78,28 +78,28 @@ describe("CREATE KEYS MNEMONIC", () => {
     });
   });
   it("createkeys mnemonic error", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --mnemonic obscure property tackle faculty fresh gas clerk order silver answer belt brother --pass password --path ./src/walletRollupMnemonic.json');
+    const out = process.exec('cd ..; node cli.js createkeys --keytype rollup --mnemonic obscure property tackle faculty fresh gas clerk order silver answer belt brother --pass password --path ./src/walletRollupMnemonic.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("createkeys mnemonic error 2", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt" --pass password --path ./src/walletRollupMnemonic.json');
+    const out = process.exec('cd ..; node cli.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt" --pass password --path ./src/walletRollupMnemonic.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("createkeys mnemonic error password", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --path ./src/walletRollupMnemonic.json');
+    const out = process.exec('cd ..; node cli.js createkeys --keytype rollup --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --path ./src/walletRollupMnemonic.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("createkeys mnemonic error keytype", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --pass password --path ./src/walletRollupMnemonic.json');
+    const out = process.exec('cd ..; node cli.js createkeys --mnemonic "obscure property tackle faculty fresh gas clerk order silver answer belt brother" --pass password --path ./src/walletRollupMnemonic.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
@@ -109,7 +109,7 @@ describe("CREATE KEYS MNEMONIC", () => {
 
 describe("CREATE KEYS IMPORT", () => {
   it("createkeys import", (done) => {
-    process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --import ./src/wallet-test.json --pass password --path ./src/walletRollupImport.json', () => {
+    process.exec('cd ..; node cli.js createkeys --keytype rollup --import ./src/wallet-test.json --pass password --path ./src/walletRollupImport.json', () => {
       var readWalletImport = undefined;
       readWalletImport = fs.readFileSync('../src/walletRollupImport.json', "utf8");
       expect(JSON.parse(readWalletImport).ethWallet.address).to.be.equal("8c7da02a293238b03d6b84963a243b61fe02ae78");
@@ -118,7 +118,7 @@ describe("CREATE KEYS IMPORT", () => {
     });
   });
   it("createkeys import default path", (done) => {
-    process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --import ./src/wallet-test.json --pass password', () => {
+    process.exec('cd ..; node cli.js createkeys --keytype rollup --import ./src/wallet-test.json --pass password', () => {
       var readWalletImport = undefined;
       readWalletImport = fs.readFileSync(walletPathDefault, "utf8");
       expect(JSON.parse(readWalletImport).ethWallet.address).to.be.equal("8c7da02a293238b03d6b84963a243b61fe02ae78");
@@ -127,21 +127,21 @@ describe("CREATE KEYS IMPORT", () => {
     });
   });
   it("createkeys import error", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --import ./src/wallet-testttt.json --pass password --path ./src/walletRollupImport.json');
+    const out = process.exec('cd ..; node cli.js createkeys --keytype rollup --import ./src/wallet-testttt.json --pass password --path ./src/walletRollupImport.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("createkeys import error password", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --keytype rollup --import ./src/wallet-test.json --path ./src/walletRollupImport.json');
+    const out = process.exec('cd ..; node cli.js createkeys --keytype rollup --import ./src/wallet-test.json --path ./src/walletRollupImport.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("createkeys import error keytype", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js createkeys --import ./src/wallet-test.json --pass password --path ./src/walletRollupImport.json');
+    const out = process.exec('cd ..; node cli.js createkeys --import ./src/wallet-test.json --pass password --path ./src/walletRollupImport.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
@@ -151,42 +151,42 @@ describe("CREATE KEYS IMPORT", () => {
 
 describe("ONCHAINTX", () => {
   it("onchaintx deposit", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js onchaintx --type deposit --pass password --amount 3 --tokenid 1234 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js onchaintx --type deposit --pass password --amount 3 --tokenid 1234 --paramsTx ./src/config-example.json');
     out.stdout.on('data', (data) => {
       expect("deposit\n").to.be.equal(data);
       done();
     });
   });
   it("onchaintx deposit default config.json", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js onchaintx --type deposit --pass password --amount 3 --tokenid 1234');
+    const out = process.exec('cd ..; node cli.js onchaintx --type deposit --pass password --amount 3 --tokenid 1234');
     out.stdout.on('data', (data) => {
       expect("deposit\n").to.be.equal(data);
       done();
     });
   });
   it("onchaintx deposit error pass", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js onchaintx --type deposit --amount 3 --tokenid 1234 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js onchaintx --type deposit --amount 3 --tokenid 1234 --paramsTx ./src/config-example.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("onchaintx deposit error amount", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js onchaintx --type deposit --pass password --tokenid 1234 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js onchaintx --type deposit --pass password --tokenid 1234 --paramsTx ./src/config-example.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("onchaintx deposit error token id", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js onchaintx --type deposit --pass password --amount 3 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js onchaintx --type deposit --pass password --amount 3 --paramsTx ./src/config-example.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("onchaintx deposit error config file", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js onchaintx --type deposit --pass password --amount 3 --paramsTx ./src/config-examplee.json');
+    const out = process.exec('cd ..; node cli.js onchaintx --type deposit --pass password --amount 3 --paramsTx ./src/config-examplee.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
@@ -196,42 +196,42 @@ describe("ONCHAINTX", () => {
 
 describe("OFFCHAINTX", () => {
   it("offchaintx send", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js offchaintx --type send --pass password --amount 3 --to 1111 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js offchaintx --type send --pass password --amount 3 --to 1111 --paramsTx ./src/config-example.json');
     out.stdout.on('data', (data) => {
       expect("send\n").to.be.equal(data);
       done();
     });
   });
   it("offchaintx send default config.json", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js offchaintx --type send --pass password --amount 3 --to 1111');
+    const out = process.exec('cd ..; node cli.js offchaintx --type send --pass password --amount 3 --to 1111');
     out.stdout.on('data', (data) => {
       expect("send\n").to.be.equal(data);
       done();
     });
   });
   it("offchaintx send error pass", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js offchaintx --type send --amount 3 --to 1111 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js offchaintx --type send --amount 3 --to 1111 --paramsTx ./src/config-example.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("offchaintx send error amount", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js offchaintx --type send --pass password --to 1111 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js offchaintx --type send --pass password --to 1111 --paramsTx ./src/config-example.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("offchaintx send error recipient", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js offchaintx --type send --pass password --amount 3 --paramsTx ./src/config-example.json');
+    const out = process.exec('cd ..; node cli.js offchaintx --type send --pass password --amount 3 --paramsTx ./src/config-example.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
     });
   });
   it("offchaintx send error config file", (done) => {
-    const out = process.exec('cd ..; node cli-laia.js offchaintx --type send --pass password --amount 3 --to 1111 --paramsTx ./src/config-examplee.json');
+    const out = process.exec('cd ..; node cli.js offchaintx --type send --pass password --amount 3 --to 1111 --paramsTx ./src/config-examplee.json');
     out.on('exit', (code) => {
       expect(code).to.not.be.equal(0);
       done();
