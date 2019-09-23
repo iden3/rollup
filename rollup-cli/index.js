0ea3fdd351bd43c705544f0c6e6da87acc2f1a10
Laia <laisolizq@gmail.com>
2019-09-23 10:30:53 +0200

    little changes

--- rollup-cli/index.js
+++ rollup-cli/index.js
@@ -1,9 +1,9 @@
 const Db = require('./src/db');
-//const KeyContainer = require('./src/kc');
+// const KeyContainer = require('./src/kc');
 const cliUtils = require('./src/cli-utils');
 
 module.exports = {
   Db,
- // KeyContainer,
+  // KeyContainer,
   cliUtils,
 };
--- rollup-cli/src/actions/onchain/deposit.js
+++ rollup-cli/src/actions/onchain/deposit.js
@@ -1,21 +1,21 @@
 const ethers = require('ethers');
 const { BabyJubWallet } = require('../../../../rollup-utils/babyjub-wallet');
 
-async function deposit(urlNodo, addressSC, balance, tokenId, walletEthJson, BabyjubJson, password, abi)  {
+async function deposit(urlNodo, addressSC, balance, tokenId, walletEthJson, BabyjubJson, password, abi) {
   const provider = new ethers.providers.JsonRpcProvider(urlNodo);
   let wallet = await ethers.Wallet.fromEncryptedJson(walletEthJson, password);
-  let walletBaby = await BabyJubWallet.fromEncryptedJson(BabyjubJson, password);
-  let pubKeyBabyjub = [walletBaby.publicKey[0].toString(), walletBaby.publicKey[1].toString()];
+  const walletBaby = await BabyJubWallet.fromEncryptedJson(BabyjubJson, password);
+  const pubKeyBabyjub = [walletBaby.publicKey[0].toString(), walletBaby.publicKey[1].toString()];
   wallet = wallet.connect(provider);
-  let address = await wallet.getAddress()
-  let contractWithSigner = new ethers.Contract(addressSC, abi, wallet)
-  let overrides = {
+  const address = await wallet.getAddress();
+  const contractWithSigner = new ethers.Contract(addressSC, abi, wallet);
+  const overrides = {
     gasLimit: 800000,
     value: ethers.utils.parseEther('1.0'),
   };
-  try{
-    return new Promise (async function (resolve, reject){
-      let response = await contractWithSigner.deposit(balance, tokenId, pubKeyBabyjub, address, overrides); //await? promise?s
+  try {
+    return new Promise (async function (resolve){
+      const response = await contractWithSigner.deposit(balance, tokenId, pubKeyBabyjub, address, overrides); //await? promise?s
       // let wait = await response.wait()
       // console.log(wait.events)
       resolve(response);
@@ -28,6 +28,4 @@ async function deposit(urlNodo, addressSC, balance, tokenId, walletEthJson, Baby
   
 module.exports = {
     deposit
-  };
-
-  
+};
