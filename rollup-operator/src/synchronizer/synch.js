54f291e9a65a1b48a82595e125101b7cb3b538e7
krlosMata <krlosmata@gmail.com>
2019-09-25 23:28:33 +0200

    implement synchronizer. wip test

--- rollup-operator/src/synchronizer/synch.js
+++ rollup-operator/src/synchronizer/synch.js
@@ -1,13 +1,20 @@
 const Web3 = require("web3");
-const { stringifyBigInts, unstringifyBigInts } = require("snarkjs");
+const abiDecoder = require("abi-decoder");
+const rollupUtils = require("../../../rollup-utils/rollup-utils");
+const { stringifyBigInts, unstringifyBigInts, bigInt } = require("snarkjs");
 
-// globsal vars
+// global vars
 const lastBlockKey = "last-block-synch";
 const stateRootKey = "last-state-root";
 const lastBatchKey = "last-state-batch";
+const eventOnChainKey = "onChain";
+const eventForgeBatchKey = "forgeBatch";
+const separator = "--";
 const TIMEOUT_ERROR = 3000;
-const TIMEOUT_NEXT_LOOP = 15000;
-
+const TIMEOUT_NEXT_LOOP = 5000;
+const maxTx = 4;
+const nLevels = 24;
+const bytesOffChainTx = 3*2 + 2;
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -23,6 +30,7 @@ class Synchronizer {
         this.ethAddress = ethAddress;
         this.web3 = new Web3(new Web3.providers.HttpProvider(this.nodeUrl));
         this.rollupContract = new this.web3.eth.Contract(rollupABI, this.rollupAddress);
+        abiDecoder.addABI(rollupABI);
     }
 
     _toString(val) {
@@ -46,34 +54,36 @@ class Synchronizer {
                 // get last block synched and current blockchain block
                 let lastSynchBlock = await this.getLastSynchBlock();
                 const currentBlock = await this.web3.eth.getBlockNumber();
-
+                console.log("******************************");
                 console.log(`creation block: ${this.creationBlock}`);
                 console.log(`last synchronized block: ${lastSynchBlock}`);
-                console.log(`current block number: ${currentBlock}`);
+                console.log(`current block number: ${currentBlock}\n`);
 
-                if (lastSynchBlock <= this.creationBlock) {
-                    console.log("creation block to last synch block");
+                if (lastSynchBlock < this.creationBlock) {
                     await this.db.insert(lastBlockKey, this._toString(lastSynchBlock));
                     lastSynchBlock = this.creationBlock;
                 }
                 
-                const batchDepth = await this.rollupContract.methods.getStateDepth().call({from: this.ethAddress}, currentBlock);
+                const currentBatchDepth = await this.rollupContract.methods.getStateDepth().call({from: this.ethAddress}, currentBlock);
                 const lastBatchSaved = await this.getLastBatch();
 
-                if ((batchDepth != 0) && (lastBatchSaved < batchDepth)) {
-                    // const stateRoot = await this.rollupContract.methods.getStateRoot(this.batchDepth).call({from: this.ethAddress}, lastSynchBlock);
-                    // const lastRoot = await this.getStateRoot(batchDepth);
-                    console.log("IN get events");
+                console.log(`current batch depth: ${currentBatchDepth}`);
+                console.log(`last batch saved: ${lastBatchSaved}`);
+
+                if ((currentBatchDepth != 0) && (lastBatchSaved < currentBatchDepth)) {
                     const targetBlockNumber = Math.min(currentBlock, lastSynchBlock + 10);
                     const logs = await this.rollupContract.getPastEvents("allEvents", {
                         fromBlock: lastSynchBlock + 1,
                         toBlock: targetBlockNumber,
                     });
-                    await this.saveEvents(logs, batchDepth, targetBlockNumber);
-
-                } else{
+                    await this._updateEvents(logs, lastBatchSaved, currentBatchDepth, targetBlockNumber);
+                    console.log("Events synchronized correctly");
+                } else {
                     console.log("No batches has been forged");
+                    // update last block synched
+                    await this.db.insert(lastBlockKey, this._toString(currentBlock));
                 }
+                console.log("******************************\n");
                 await timeout(TIMEOUT_NEXT_LOOP);
             } catch (e) {
                 console.error(`Message error: ${e.message}`);
@@ -88,18 +98,109 @@ class Synchronizer {
     }
 
     async getBatchRoot(numBatch) {
-        return this._fromString(await this.db.getOrDefault(`${stateRootKey}-${numBatch}`, ""));
+        return this._fromString(await this.db.getOrDefault(`${stateRootKey}${separator}${numBatch}`, "0"));
     }
 
     async getLastBatch(){
         return this._fromString(await this.db.getOrDefault(lastBatchKey, "0"));
     }
 
-    async saveEvents(logs, batchDepth, blockNumber){
-        console.log(logs);
+    async _updateEvents(logs, lastBatchSaved, currentBatchDepth, blockNumber){
+        // save events on database
+        logs.forEach((elem, index )=> {
+            this._saveEvents(elem, index);
+        });
+        // Update rollupTree and last batch synched
+        for (let i = lastBatchSaved + 1; i <= currentBatchDepth; i++) {
+            const eventForge = [];
+            const eventOnChain = [];
+            // add off-chain tx
+            const keysForge = await this.db.listKeys(`${eventForgeBatchKey}${separator}${i-1}`);
+            for (const key of keysForge) eventForge.push(this._fromString(await this.db.get(key)));
+            // add on-chain tx
+            const keysOnChain = await this.db.listKeys(`${eventOnChainKey}${separator}${i-2}`);
+            for (const key of keysOnChain) eventOnChain.push(this._fromString(this.db.get(key)));
+            // Add events to rollup-tree
+            await this._updateTree(eventForge, eventOnChain);
+        }
         await this.db.insert(lastBlockKey, this._toString(blockNumber));
-        await this.db.insert(lastBatchKey, this._toString(batchDepth));
+        await this.db.insert(lastBatchKey, this._toString(currentBatchDepth));
     }
+
+    async _saveEvents(event, index) {
+        if (event.event == "OnChainTx") {
+            const batchNumber = event.returnValues.batchNumber;
+            await this.db.insert(`${eventOnChainKey}${separator}${batchNumber}${separator}${index}`,
+                this._toString(event.returnValues));
+        } else if(event.event == "ForgeBatch") {
+            const batchNumber = event.returnValues.batchNumber;
+            await this.db.insert(`${eventForgeBatchKey}${separator}${batchNumber}${separator}${index}`,
+                this._toString(event.returnValues));
+        }
+    }
+
+    async _updateTree(offChain, onChain) {
+        const block = await this.treeDb.buildBlock(maxTx, nLevels);
+        for (const event of offChain) {
+            const offChainTxs = await this._getTxOffChain(event);
+            for (const tx of offChainTxs) block.addTx(tx);
+        }
+        for (const event of onChain) {
+            block.addTx(await this._getTxOnChain(event));
+        }
+        await block.build();
+        await this.treeDb.consolidate(block);
+    }
+
+    async _getTxOnChain(event) {
+        const txData = rollupUtils.decodeTxData(event.txData);
+        return {
+            fromIdx: txData.fromId,
+            toIdx: txData.toId,
+            amount: txData.amount,
+            loadAmount: bigInt(event.loadAmount),
+            coin: txData.tokenId,
+            ax: bigInt(event.Ax).toString(16),
+            ay: bigInt(event.Ay).toString(16),
+            ethAddress: bigInt(event.ethAddress).toString(),
+            onChain: true
+        };
+    }
+
+    async _getTxOffChain(event) {
+        const transaction = await this.web3.eth.getTransactionFromBlock(event.blockNumber.toString(), 0);
+        const decodedData = abiDecoder.decodeMethod(transaction.input);
+        let compressedTxs;
+        for (const elem of decodedData.params) {
+            if (elem.name == "compressedTxs") compressedTxs = elem.value;
+        }
+        const txs = [];
+        if (compressedTxs != null){
+            const buffTxs = Buffer.from(compressedTxs.slice(2), "hex");
+            const nTx = buffTxs.length / bytesOffChainTx;
+            for (let i = 0; i < nTx; i++) {
+                const tx = {
+                    fromIdx: buffTxs.readUIntBE(8*i, 3),
+                    toIdx: buffTxs.readUIntBE(8*i + 3, 3),
+                    amount: buffTxs.readUIntBE(8*i + 6, 2),
+                };
+                txs.push(tx);
+            }
+        }
+        return txs;
+    }
+
+    getState() {
+        return this.treeDb.db.nodes;
+    }
+
+    // async getIdInfo(id) {
+    //     const resFind = await this.smt.find(id);
+    //     if (resFind.found) {
+    //         resFind.foundObject = this._fromString(await this.leafDb.get(resFind.foundValue));
+    //     }
+    //     return resFind;
+    // }
 }
 
 module.exports = Synchronizer;
--- rollup-operator/test/synch.test.js
+++ rollup-operator/test/synch.test.js
@@ -1,77 +1,187 @@
 /* global artifacts */
 /* global contract */
 /* global web3 */
+/* global BigInt */
 
 const chai = require("chai");
-const process = require("child_process");
 const { expect } = chai;
-const EventTest = artifacts.require("../../contracts/test/EventTest");
+const poseidonUnit = require("circomlib/src/poseidon_gencontract");
+const TokenRollup = artifacts.require("../contracts/test/TokenRollup");
+const Verifier = artifacts.require("../contracts/test/VerifierHelper");
+const StakerManager = artifacts.require("../contracts/RollupPoS");
+const RollupTest = artifacts.require("../contracts/test/RollupTest");
 const Synchronizer = require("../src/synchronizer/synch");
-const RollupTree = require("../../rollup-utils/rollup-tree");
-const LevelDb = require("../../rollup-utils/level-db");
+const MemDb = require("../../rollup-utils/mem-db");
+const RollupDB = require("../../js/rollupdb");
+const SMTMemDB = require("circomlib/src/smt_memdb");
+const rollupUtils = require("../../rollup-utils/rollup-utils");
+const { BabyJubWallet } = require("../../rollup-utils/babyjub-wallet");
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function buildInputSm(bb) {
+    const feePlan = rollupUtils.buildFeeInputSm(bb.feePlan);
+    return [
+        bb.getInput().oldStRoot.toString(),
+        bb.getNewStateRoot().toString(),
+        bb.getNewExitRoot().toString(),
+        bb.getOnChainHash().toString(),
+        bb.getOffChainHash().toString(),
+        feePlan[0],
+        feePlan[1],
+        bb.getCountersOut().toString(),
+    ];
+}
+
+function manageEvent(event) {
+    if (event.event == "OnChainTx") {
+        const txData = rollupUtils.decodeTxData(event.args.txData);
+        return {
+            fromIdx: txData.fromId,
+            toIdx: txData.toId,
+            amount: txData.amount,
+            loadAmount: BigInt(event.args.loadAmount),
+            coin: txData.tokenId,
+            ax: BigInt(event.args.Ax).toString(16),
+            ay: BigInt(event.args.Ay).toString(16),
+            ethAddress: BigInt(event.args.ethAddress).toString(),
+            onChain: true
+        };
+    }
+}
+
 contract("Synchronizer", (accounts) => {
+    
+    async function forgeBlock(events = undefined) {
+        const block = await opRollupDb.buildBlock(maxTx, nLevels);
+        if (events) {
+            events.forEach(elem => {
+                block.addTx(manageEvent(elem));
+            });
+        }
+        await block.build();
+        const inputSm = buildInputSm(block, beneficiary);
+        const compressedTx = `0x${block.getDataAvailable().toString("hex")}`;
+        await insRollupTest.forgeBatch(beneficiary, proofA, proofB, proofC, inputSm, compressedTx);
+        await opRollupDb.consolidate(block);
+    }
+
     const {
         0: owner,
-        1: synchAddress,
+        1: id1,
+        2: synchAddress,
+        3: beneficiary,
     } = accounts;
-    let insEvent;
-    let rollupTree;
+
     let synchDb;
     let synch;
-    let stateRoots = [];
+
+    const maxTx = 10;
+    const maxOnChainTx = 3;
+    const nLevels = 24;
+    const tokenInitialAmount = 1000;
+    const tokenId = 0;
+
+    // Operator databse
+    let opDb;
+    let opRollupDb;
+
+    // Synchronizer database
+    let db;
+    let synchRollupDb;
+
+    let insPoseidonUnit;
+    let insTokenRollup;
+    let insStakerManager;
+    let insRollupTest;
+    let insVerifier;
+
     const pathDb = `${__dirname}/tmp`;
 
     let configSynch = {
         pathTreeDb: pathDb,
         pathSynchDb: `${pathDb}-synch`,
         ethNodeUrl: "http://localhost:8545",
-        eventsAddress: undefined,
+        contractAddress: undefined,
         creationHash: undefined,
         ethAddress: synchAddress,
-        abi: EventTest.abi,
+        abi: RollupTest.abi,
     }; 
 
+    // BabyJubjub public key
+    const mnemonic = "urban add pulse prefer exist recycle verb angle sell year more mosquito";
+    const wallet = BabyJubWallet.fromMnemonic(mnemonic);
+    const Ax = wallet.publicKey[0].toString();
+    const Ay = wallet.publicKey[1].toString();
+
+    // Fake proofs
+    const proofA = ["0", "0"];
+    const proofB = [["0", "0"], ["0", "0"]];
+    const proofC = ["0", "0"];
+
     before(async () => {
-        // Deploy token test
-        insEvent = await EventTest.new();
+        // Deploy poseidon
+        const C = new web3.eth.Contract(poseidonUnit.abi);
+        insPoseidonUnit = await C.deploy({ data: poseidonUnit.createCode() })
+            .send({ gas: 2500000, from: owner });
+
+        // Deploy TokenRollup
+        insTokenRollup = await TokenRollup.new(id1, tokenInitialAmount);
+
+        // Deploy Verifier
+        insVerifier = await Verifier.new();
+
+        // Deploy Rollup test
+        insRollupTest = await RollupTest.new(insVerifier.address, insPoseidonUnit._address,
+            maxTx, maxOnChainTx);
+
+        // Deploy Staker manager
+        insStakerManager = await StakerManager.new(insRollupTest.address);
+
+        // load forge batch mechanism ( not used in this test)
+        await insRollupTest.loadForgeBatchMechanism(insStakerManager.address);
+
         // load configuration synchronizer
-        configSynch.eventsAddress = insEvent.address;
-        configSynch.creationHash = insEvent.transactionHash;
-        // Test state roots
-        for (let i = 0; i < 50; i++) {
-            stateRoots.push(`0x${Buffer.alloc(32).fill(i, 31).toString("hex")}`);
-        }
-    });
-
-    after(async () => {
-        timeout(20000);
-        after(async () => {
-            process.exec(`rm -rf ${pathDb}-leafs`);
-            process.exec(`rm -rf ${pathDb}-tree`);
-            process.exec(`rm -rf ${pathDb}-synch`);
-        });
-    });
-
-    it("initialize synchronizer", async () => {
-        rollupTree = await RollupTree.newLevelDbRollupTree(configSynch.pathTreeDb);
-        synchDb = new LevelDb(configSynch.pathSynchDb);
+        configSynch.contractAddress = insRollupTest.address;
+        configSynch.creationHash = insRollupTest.transactionHash;
         
-        synch = new Synchronizer(synchDb, rollupTree, configSynch.ethNodeUrl,
-            configSynch.eventsAddress, configSynch.abi, configSynch.creationHash, configSynch.ethAddress);
+        // Init Synch Rollup databases
+        db = new SMTMemDB();
+        synchRollupDb = await RollupDB(db);
+        // Init operator Rollup Database
+        opDb = new SMTMemDB();
+        opRollupDb = await RollupDB(opDb);
     });
 
-    it("forge batches", async () => {
-        for (let i = 0; i < 1; i++) {
-            await insEvent.forgeBatch(stateRoots[i], "0x00000000");    
-        }
+    it("manage rollup token", async () => { 
+        await insRollupTest.addToken(insTokenRollup.address,
+            { from: id1, value: web3.utils.toWei("1", "ether") });
+
+        await insTokenRollup.approve(insRollupTest.address, tokenInitialAmount,
+            { from: id1 });
     });
 
-    it("loop synchronize events", async () => {
-        await synch.synchLoop();
+    it("Should initialize synchronizer", async () => {
+        synchDb = new MemDb();
+        synch = new Synchronizer(synchDb, synchRollupDb, configSynch.ethNodeUrl,
+            configSynch.contractAddress, configSynch.abi, configSynch.creationHash, configSynch.ethAddress);
+    });
+
+    it("Should add deposits and synchronize", async () => {
+        const loadAmount = 10;
+        const event = await insRollupTest.deposit(loadAmount, tokenId, id1,
+            [Ax, Ay], { from: id1, value: web3.utils.toWei("1", "ether") });
+        await forgeBlock();
+        await forgeBlock([event.logs[0]]);
+    });
+
+    it("Should start synchronizer", async () => {
+        synch.synchLoop();
+    });
+
+    it("timeout test", async () => {
+        await timeout(10000);
     });
 });
