a995fb70c8c6aa4e1993b9ce59e3fd03e1acca02
krlosMata <krlosmata@gmail.com>
2020-02-12 14:15:36 +0100

    server proof with large payload data

--- rollup-operator/src/server-proof.js
+++ rollup-operator/src/server-proof.js
@@ -62,7 +62,8 @@ async function genProof() {
 }
 ///// Server configuration
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
