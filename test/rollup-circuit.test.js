3e54e1e99f0be3d2272f79fec68b1bcc14821d47
laisolizq <laisolizq@gmail.com>
2020-04-09 17:53:24 +0200

    add tests

--- test/rollup-circuit.test.js
+++ test/rollup-circuit.test.js
@@ -1981,4 +1981,386 @@ describe("Rollup Basic circuit TXs", function () {
         }
     });
 
+    it("Should create 1 deposit on-chain and then should check error 1 force-withdraw on-chain with invalid txData", async () => {
+        // Start a new state
+        const db = new SMTMemDB();
+        const rollupDB = await RollupDB(db);
+        const bb = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        const account1 = new RollupAccount(1);
+
+        bb.addTx({
+            loadAmount: 1000,
+            coin: 0,
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            onChain: true
+        });
+
+        await bb.build();
+        await rollupDB.consolidate(bb);
+
+        const bb2 = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        bb2.addTx({
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            coin: 0,
+            amount: 500,
+            nonce: 0,
+            onChain: true
+        });
+
+        await bb2.build();
+        await rollupDB.consolidate(bb2);
+        const input = bb2.getInput();
+
+        // manipulate input
+        const tx = {
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            coin: 0,
+            amount: 1000,
+            nonce: 0,
+            onChain: true
+        };
+        let newAccount = 0;
+        const txData = utils.buildTxData(Object.assign({newAccount: newAccount}, tx));
+        input.txData[0] = txData;
+
+        try {
+            circuit.calculateWitness(input, {logTrigger:false, logOutput: false, logSet: false});
+            assert(false);
+        } catch (error) {
+            assert.include(error.message, "doesn't match main");
+        }
+    });
+
+    it("Should create 1 deposit on-chain and then should check error 1 force-withdraw on-chain with invalid from", async () => {
+        // Start a new state
+        const db = new SMTMemDB();
+        const rollupDB = await RollupDB(db);
+        const bb = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        const account1 = new RollupAccount(1);
+        const account2 = new RollupAccount(2);
+
+        bb.addTx({
+            loadAmount: 1000,
+            coin: 0,
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            onChain: true
+        });
+
+        await bb.build();
+        await rollupDB.consolidate(bb);
+
+        const bb2 = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        bb2.addTx({
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            coin: 0,
+            amount: 500,
+            nonce: 0,
+            onChain: true
+        });
+
+        await bb2.build();
+        await rollupDB.consolidate(bb2);
+        const input = bb2.getInput();
+
+        // manipulate input
+        input.fromAx[0] = bigInt("0x" + account2.ax);
+        input.fromAy[0] = bigInt("0x" + account2.ay);
+        input.fromEthAddr[0] = bigInt(account2.ethAddress);
+
+        try {
+            circuit.calculateWitness(input, {logTrigger:false, logOutput: false, logSet: false});
+            assert(false);
+        } catch (error) {
+            assert.include(error.message, "main.Tx[0].fromEthAddrChecker");
+        }
+
+    });
+
+    it("Should create 2 deposits on-chain and then 1 off-chain transfer with invalid order", async () => {
+        // Start a new state
+        const db = new SMTMemDB();
+        const rollupDB = await RollupDB(db);
+        const bb = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        const account1 = new RollupAccount(1);
+        const account2 = new RollupAccount(2);
+        const account3 = new RollupAccount(3);
+
+        bb.addTx({
+            loadAmount: 1000,
+            coin: 0,
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            onChain: true
+        });
+
+        bb.addTx({
+            loadAmount: 2000,
+            coin: 0,
+            fromAx: account2.ax,
+            fromAy: account2.ay,
+            fromEthAddr: account2.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            onChain: true
+        });
+
+        await bb.build();
+        await rollupDB.consolidate(bb);
+
+        const bb2 = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        bb2.addTx({
+            loadAmount: 3000,
+            coin: 0,
+            fromAx: account3.ax,
+            fromAy: account3.ay,
+            fromEthAddr: account3.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            onChain: true
+        });
+
+        const tx = {
+            toAx: account2.ax,
+            toAy: account2.ay,
+            toEthAddr: account2.ethAddress,
+            coin: 0,
+            amount: 500,
+            nonce: 0,
+            userFee: 100
+        };
+        account1.signTx(tx);
+        bb2.addTx(tx);
+
+        bb2.addCoin(0, 100);
+
+        await bb2.build();
+        await rollupDB.consolidate(bb2);
+        const input = bb2.getInput();
+    
+        // manipulate input
+        const auxInput = {};
+
+        auxInput.txData = Object.assign([], input.txData);
+        input.txData[0] = auxInput.txData[4];
+        input.txData[4] = auxInput.txData[0];
+
+        auxInput.fromIdx = Object.assign([], input.fromIdx);
+        input.fromIdx[0] = auxInput.fromIdx[4];
+        input.fromIdx[4] = auxInput.fromIdx[0];
+
+        auxInput.toIdx = Object.assign([], input.toIdx);
+        input.toIdx[0] = auxInput.toIdx[4];
+        input.toIdx[4] = auxInput.toIdx[0];
+
+        auxInput.toAx = Object.assign([], input.toAx);
+        input.toAx[0] = auxInput.toAx[4];
+        input.toAx[4] = auxInput.toAx[0];
+
+        auxInput.toAy = Object.assign([], input.toAy);
+        input.toAy[0] = auxInput.toAy[4];
+        input.toAy[4] = auxInput.toAy[0];
+
+        auxInput.toEthAddr = Object.assign([], input.toEthAddr);
+        input.toEthAddr[0] = auxInput.toEthAddr[4];
+        input.toEthAddr[4] = auxInput.toEthAddr[0];
+
+        auxInput.s = Object.assign([], input.s);
+        input.s[0] = auxInput.s[4];
+        input.s[4] = auxInput.s[0];
+
+        auxInput.r8x = Object.assign([], input.r8x);
+        input.r8x[0] = auxInput.r8x[4];
+        input.r8x[4] = auxInput.r8x[0];
+
+        auxInput.r8y = Object.assign([], input.r8y);
+        input.r8y[0] = auxInput.r8y[4];
+        input.r8y[4] = auxInput.r8y[0];
+
+        auxInput.loadAmount = Object.assign([], input.loadAmount);
+        input.loadAmount[0] = auxInput.loadAmount[4];
+        input.loadAmount[4] = auxInput.loadAmount[0];
+
+        auxInput.fromEthAddr = Object.assign([], input.fromEthAddr);
+        input.fromEthAddr[0] = auxInput.fromEthAddr[4];
+        input.fromEthAddr[4] = auxInput.fromEthAddr[0];
+
+        auxInput.fromAx = Object.assign([], input.fromAx);
+        input.fromAx[0] = auxInput.fromAx[4];
+        input.fromAx[4] = auxInput.fromAx[0];
+
+        auxInput.fromAy = Object.assign([], input.fromAy);
+        input.fromAy[0] = auxInput.fromAy[4];
+        input.fromAy[4] = auxInput.fromAy[0];
+
+        auxInput.ax1 = Object.assign([], input.ax1);
+        input.ax1[0] = auxInput.ax1[4];
+        input.ax1[4] = auxInput.ax1[0];
+
+        auxInput.ay1 = Object.assign([], input.ay1);
+        input.ay1[0] = auxInput.ay1[4];
+        input.ay1[4] = auxInput.ay1[0];
+
+        auxInput.amount1 = Object.assign([], input.amount1);
+        input.amount1[0] = auxInput.amount1[4];
+        input.amount1[4] = auxInput.amount1[0];
+
+        auxInput.nonce1 = Object.assign([], input.nonce1);
+        input.nonce1[0] = auxInput.nonce1[4];
+        input.nonce1[4] = auxInput.nonce1[0];
+
+        auxInput.ethAddr1 = Object.assign([], input.ethAddr1);
+        input.ethAddr1[0] = auxInput.ethAddr1[4];
+        input.ethAddr1[4] = auxInput.ethAddr1[0];
+
+        auxInput.siblings1 = Object.assign([], input.siblings1);
+        input.siblings1[0] = auxInput.siblings1[4];
+        input.siblings1[4] = auxInput.siblings1[0];
+
+        auxInput.oldKey1 = Object.assign([], input.oldKey1);
+        input.oldKey1[0] = auxInput.oldKey1[4];
+        input.oldKey1[4] = auxInput.oldKey1[0];
+
+        auxInput.oldValue1 = Object.assign([], input.oldValue1);
+        input.oldValue1[0] = auxInput.oldValue1[4];
+        input.oldValue1[4] = auxInput.oldValue1[0];
+
+        auxInput.ax2 = Object.assign([], input.ax2);
+        input.ax2[0] = auxInput.ax2[4];
+        input.ax2[4] = auxInput.ax2[0];
+
+        auxInput.ay2 = Object.assign([], input.ay2);
+        input.ay2[0] = auxInput.ay2[4];
+        input.ay2[4] = auxInput.ay2[0];
+
+        auxInput.amount2 = Object.assign([], input.amount2);
+        input.amount2[0] = auxInput.amount2[4];
+        input.amount2[4] = auxInput.amount2[0];
+
+        auxInput.nonce2 = Object.assign([], input.nonce2);
+        input.nonce2[0] = auxInput.nonce2[4];
+        input.nonce2[4] = auxInput.nonce2[0];
+
+        auxInput.ethAddr2 = Object.assign([], input.ethAddr2);
+        input.ethAddr2[0] = auxInput.ethAddr2[4];
+        input.ethAddr2[4] = auxInput.ethAddr2[0];
+
+        auxInput.siblings2 = Object.assign([], input.siblings2);
+        input.siblings2[0] = auxInput.siblings2[4];
+        input.siblings2[4] = auxInput.siblings2[0];
+
+        auxInput.oldKey2 = Object.assign([], input.oldKey2);
+        input.oldKey2[0] = auxInput.oldKey2[4];
+        input.oldKey2[4] = auxInput.oldKey2[0];
+
+        auxInput.oldValue2 = Object.assign([], input.oldValue2);
+        input.oldValue2[0] = auxInput.oldValue2[4];
+        input.oldValue2[4] = auxInput.oldValue2[0];
+
+        try {
+            circuit.calculateWitness(input, {logTrigger:false, logOutput: false, logSet: false});
+            assert(false);
+        } catch (error) {
+            assert.include(error.message, "main.decodeTx[4]");
+        }
+    });
+
+    it("Should create 1 deposit on-chain and then 2 force-withdraw on-chain, one of them invalid", async () => {
+        // Start a new state
+        const db = new SMTMemDB();
+        const rollupDB = await RollupDB(db);
+        const bb = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        const account1 = new RollupAccount(1);
+
+        bb.addTx({
+            loadAmount: 1000,
+            coin: 0,
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            onChain: true
+        });
+
+        await bb.build();
+        await rollupDB.consolidate(bb);
+
+        const bb2 = await rollupDB.buildBatch(NTX, NLEVELS);
+
+        bb2.addTx({
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            coin: 0,
+            amount: 800,
+            nonce: 0,
+            onChain: true
+        });
+
+        bb2.addTx({
+            fromAx: account1.ax,
+            fromAy: account1.ay,
+            fromEthAddr: account1.ethAddress,
+            toAx: 0,
+            toAy: 0,
+            toEthAddr: 0,
+            coin: 0,
+            amount: 400,
+            nonce: 0,
+            onChain: true
+        });
+
+        await bb2.build();
+        await rollupDB.consolidate(bb2);
+        const input = bb2.getInput();
+
+        const w2 = circuit.calculateWitness(input, {logTrigger:false, logOutput: false, logSet: false});
+        checkBatch(circuit, w2, bb2);
+        
+        const state = await rollupDB.getStateByIdx(1);
+        assert.equal(state.amount.toString(), 200);
+    });
+
 });
