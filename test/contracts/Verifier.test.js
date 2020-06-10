f40883e3f3f892267d26e0c4acc21f6741fbc5c8
krlosMata <krlosmata@gmail.com>
2020-06-10 09:32:31 +0200

    update verifier 128 nTx 24 nLevels

--- test/contracts/Verifier.test.js
+++ test/contracts/Verifier.test.js
@@ -3,17 +3,20 @@
 
 const chai = require("chai");
 const { expect } = chai;
-const Verifier = artifacts.require("../contracts/verifiers/Verifier_16_24");
+const Verifier16 = artifacts.require("../contracts/verifiers/Verifier_16_24");
+const Verifier128 = artifacts.require("../contracts/verifiers/Verifier_128_24");
 
 const { generateCall } = require("../../rollup-operator/src/utils");
 
 contract("Verifier", () => {
 
-    let insVerifier;
+    let insVerifier16;
+    let insVerifier128;
 
-    let proof;
+    let proof16;
+    let proof128;
 
-    const proofServer = {
+    const proofServer16 = {
         proofA: [
             "9253558836951391180215401092285942392106850777386280952695062100433051361088",
             "18697207118335119388548517833235335684285063697800660628381013502181764601642"
@@ -46,33 +49,87 @@ contract("Verifier", () => {
         ],
     };
 
+    const proofServer128 = {
+        proofA: [
+            "16800683322358729782558539405444460657095236755164804941741334024853300252926",
+            "13637424009861970428436032241205875210112061894739973602480381219489614968408"
+        ],
+        proofB: [
+            [
+                "20188414792447662050888918983778584008338108248112647856696049252327606701446",
+                "16271649501527115135443119169398969989740342088044025504740081638310753880036"
+            ],
+            [
+                "15629987825442031163542187923243139838847837497408982898813570345424325112877",
+                "3609095780336634668540313867001449384068132366490686870759313434627663556514"
+            ]
+        ],
+        proofC: [
+            "3196470311703647745929562460070537579992309515710537067718315974586923451124",
+            "3097275469822708168033734516791292342184892905177629377823920901988464729120",
+        ],
+        publicInputs: [
+            "0",
+            "0",
+            "0",
+            "0",
+            "6446566627056056266281880624075440234336819950888749561686198322173882165734",
+            "0",
+            "0",
+            "0",
+            "1809279001739708310272933373752901772209482937325336653336454810774441821184",
+            "103929005321308650693500321209372945468207036570"
+        ],
+    };
+
     const inputChange = "0x000000000000000000000000123456789abcdef123456789abcdef123456789b";
 
     before(async () => {
     // Deploy new contract
-        insVerifier = await Verifier.new();
+        insVerifier16 = await Verifier16.new();
+        insVerifier128 = await Verifier128.new();
     });
 
     it("should generate input parameters for contract", async () => {
-        proof = generateCall(proofServer);
+        proof16 = generateCall(proofServer16);
+        proof128 = generateCall(proofServer128);
     });
 
-    it("verify correct proof", async () => {
-        const res = await insVerifier.verifyProof(proof.proofA, proof.proofB, proof.proofC,
-            proof.publicInputs);
+    it("verify correct proof 16", async () => {
+        const res = await insVerifier16.verifyProof(proof16.proofA, proof16.proofB, proof16.proofC,
+            proof16.publicInputs);
         expect(res).to.be.equal(true);
 
-        const gasVerify = (await insVerifier.verifyProof.estimateGas(proof.proofA, proof.proofB, proof.proofC,
-            proof.publicInputs)).toString();
+        const gasVerify = (await insVerifier16.verifyProof.estimateGas(proof16.proofA, proof16.proofB, proof16.proofC,
+            proof16.publicInputs)).toString();
         
-        console.log("Gas spend by \"VerifyProof\" function: ", gasVerify);
+        console.log("Gas spend by \"VerifyProof16\" function: ", gasVerify);
     });
 
-    it("verify incorrect proof", async () => {
-        proof.publicInputs[9] = inputChange;
+    it("verify incorrect proof 16", async () => {
+        proof16.publicInputs[9] = inputChange;
 
-        const res = await insVerifier.verifyProof(proof.proofA, proof.proofB, proof.proofC,
-            proof.publicInputs);
+        const res = await insVerifier16.verifyProof(proof16.proofA, proof16.proofB, proof16.proofC,
+            proof16.publicInputs);
+        expect(res).to.be.equal(false);
+    });
+
+    it("verify correct proof 128", async () => {
+        const res = await insVerifier128.verifyProof(proof128.proofA, proof128.proofB, proof128.proofC,
+            proof128.publicInputs);
+        expect(res).to.be.equal(true);
+
+        const gasVerify = (await insVerifier128.verifyProof.estimateGas(proof128.proofA, proof128.proofB, proof128.proofC,
+            proof128.publicInputs)).toString();
+        
+        console.log("Gas spend by \"VerifyProof128\" function: ", gasVerify);
+    });
+
+    it("verify incorrect proof 128", async () => {
+        proof128.publicInputs[9] = inputChange;
+
+        const res = await insVerifier128.verifyProof(proof128.proofA, proof128.proofB, proof128.proofC,
+            proof128.publicInputs);
         expect(res).to.be.equal(false);
     });
 });
